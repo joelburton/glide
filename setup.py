2bc3426c2901cdea05de8933174e9578a265e118
Joel Burton <joel@joelburton.com>
2019-07-05 18:36:14 -0700

    Require Sphinx 2.1.x

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@
 from setuptools import setup
 
 setup(name='Glide',
-      version='1.3',
+      version='1.4',
       description='Curriculum presentation system.',
       author='Joel Burton',
       author_email='joel@joelburton.com',
       url='https://github.com/joelburton/glide',
       packages=['glide', 'glide/directives', 'glide/writers'],
       python_requires=">= 3.6.0",
-      install_requires=["Sphinx (>=1.8.2,<1.9)"],
+      install_requires=["Sphinx (>=2.1.2,<2.2)"],
       include_package_data=True,
       package_data={'themes': ['*']}
      )
