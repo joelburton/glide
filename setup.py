49e4a7be7328b407c63cad5e7558ad186fc51c31
Joel Burton <joel@joelburton.com>
2023-07-31 13:42:49 -0700

    FEAT: mark code-font-size as !important

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup
 
 setup(
     name='Glide',
-    version='2.4',
+    version='2.4.1',
     description='Curriculum presentation system.',
     author='Joel Burton',
     author_email='joel@joelburton.com',
